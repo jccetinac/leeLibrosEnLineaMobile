bf8e642ef7f6e613ded819c3d212f04a7ac70dca
LauGalviz <galvizlaura69@gmail.com>
2023-04-06 09:51:04 -0500

    initial react native

---
+++ App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, Text, useColorScheme, View,} from 'react-native';
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
+
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+           <Text>inicio</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+});
+
+export default App;
